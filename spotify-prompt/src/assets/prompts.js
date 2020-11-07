b5c181f962e083f36e0b93eac1b5a55d1c494295
Jianna Caronan <jiannacaronan@Jiannas-MacBook-Pro.local>
2020-11-07 18:35:21 -0500

    added prompts

--- spotify-prompt/src/assets/prompts.js
+++ spotify-prompt/src/assets/prompts.js
@@ -1,7 +1,9 @@
 export const prompts = [
-    "hello",
-    "testing",
-    "do i work?",
-    "yes",
-    "hi"
+    "Use the lyrics to write about a couple.",
+    "Write a story using this as a theme song.",
+    "Write about the character(s) in this song.",
+    "Tell the story behind this song.",
+    "Your main character uses this song to impress someone. How do they respond?",
+    "Write in first person using the lyrics as your character's emotions.",
+    "How does this song make you feel? Write about a situation that would evoke the same feelings."
 ]
