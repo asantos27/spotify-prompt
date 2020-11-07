88c8e8a4fa5f3df317f98bc189e546094f3e424f
Jianna Caronan <jiannacaronan@Jiannas-MacBook-Pro.local>
2020-11-07 17:28:22 -0500

    create header div

--- spotify-prompt/src/App.js
+++ spotify-prompt/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './App.css';
+import './pretty.css';
+import Spot from './assets/Spotify_Logo_RGB_Green.png';
 import PromptPage from './components/PromptPage';
 
 import { Switch, Route } from 'react-router-dom';
@@ -7,7 +8,10 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <h1> Spotify Prompt Generator </h1>
+      <div className="title">
+      <h2>powered by <a href="http://api.spotify.com"><img src={Spot} height={18}/></a></h2>
+      <h1> Writing Prompt Generator </h1>
+      </div>
       <PromptPage/> 
     </div>
   );
