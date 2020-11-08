5c5684aabee752ddac585588425c611aae642136
Alyssa <alyssantos0406@gmail.com>
2020-11-07 22:15:27 -0500

    fixed next button

--- spotify-prompt/src/components/Generator.js
+++ spotify-prompt/src/components/Generator.js
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
 
 const Generator = (props) => {
-
   const generate = () => {
     return props.dataset[Math.floor(Math.random() * props.dataset.length)];
   }
 
   const [prompt, setPrompt] = useState(() => generate())
 
-
   return (
     <div>
-      <p id="prompt">{prompt}</p>
       <button class="button" onClick={() => setPrompt(generate())}><span>Next </span></button>
+      <p id="prompt">{prompt}</p>
     </div>
   );
 }
