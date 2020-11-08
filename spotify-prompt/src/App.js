7c8b6fd74f7e72080a1b4821797c95023c49106c
Jianna Caronan <jiannacaronan@Jiannas-MacBook-Pro.local>
2020-11-08 11:22:02 -0500

    latest update

--- spotify-prompt/src/App.js
+++ spotify-prompt/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div id="container">
       <div className="title">
-      <h2>powered by <a href="http://api.spotify.com"><img src={Spot} height={18}/></a></h2>
+      <h2>powered by <a href="http://api.spotify.com"><img src={Spot} alt="Spotify" height={18}/></a></h2>
       <h1> Writing Prompt Generator </h1>
       <p3> Music-inspired writing prompts to help beat writer's block </p3>
       </div>
